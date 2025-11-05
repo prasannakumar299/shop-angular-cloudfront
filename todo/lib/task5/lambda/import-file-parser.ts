e199d7fe61b3853734f08fc4b8a85c3bf852bc66
Merugumuvvala Prasanna <merugumuvvala_prasanna@epam.com>
2025-11-05 23:37:25 +0530

    feat(db): sqs and sns flow

--- todo/lib/task5/lambda/import-file-parser.ts
+++ todo/lib/task5/lambda/import-file-parser.ts
@@ -1,8 +1,11 @@
 import { S3Event } from 'aws-lambda';
-import { S3 } from 'aws-sdk';
+import { S3, SQS } from 'aws-sdk';
 import csv from 'csv-parser';
 
 const s3 = new S3();
+const sqs = new SQS();
+
+const SQS_URL = process.env.SQS_URL!;
 
 export const handler = async (event: S3Event) => {
   console.log('Received S3 event:', JSON.stringify(event, null, 2));
@@ -25,20 +28,30 @@ export const handler = async (event: S3Event) => {
       await new Promise<void>((resolve, reject) => {
         s3Stream
           .pipe(csv())
-          .on('data', (data) => {
-            console.log('Parsed record:', data); //  each row logged to CloudWatch
+          .on('data', async (data) => {
+            try {
+              await sqs
+                .sendMessage({
+                  QueueUrl: SQS_URL,
+                  MessageBody: JSON.stringify(data),
+                })
+                .promise();
+              console.log('📤 Sent record to SQS:', data);
+            } catch (err) {
+              console.error('❌ Failed to send record to SQS:', err);
+            }
           })
           .on('end', () => {
             console.log('✅ CSV parsing completed.');
             resolve();
           })
           .on('error', (err) => {
-            console.error(' Error parsing CSV:', err);
+            console.error('❌ Error parsing CSV:', err);
             reject(err);
           });
       });
     } catch (error) {
-      console.error(' Failed to process file:', error);
+      console.error('❌ Failed to process file:', error);
       throw error;
     }
   }
